0a7ed2a914362a785c321824e76b031e0da12b39
Josue Ttito <josue.ttito@ucsp.edu.pe>
2013-11-04 13:46:50 -0500

    New Changes on structure - The graph is now a dictionary - The add_nodes_from_list functions is removed - The make_full_connected function now receives an array - New function nodes that returns an array of nodes. - New function edges that returns an array of tuples (distance, from node, to node) - Remove Node and Edge structure

--- graphx/graphx.py
+++ graphx/graphx.py
@@ -1,192 +1,82 @@
 from random import random, choice, randint
-from copy import copy
-from collections import deque, Counter
+from collections import Counter
 
-class Node(object):
-	def __init__ (self, data):
-		self.data = data
-		self.edges = set()
+def graph_to_tuple(graph, key = lambda t: t[0]):
+	return sorted([ (graph[node][edge], node, edge) for node in graph.keys() for edge in graph[node] ], key = key)
 
-	def __repr__ (self):
-		return str (self.data)
-
-	def __cmp__ (self, other):
-		if type (other) == Node:
-			return cmp(self.data, other.data)
-		else:
-			raise TypeError("Node cannot be compared to %s" % str(type(other)))
-
-	def __eq__(self, other): 
-		if type(other) == Node and cmp(self.data, other.data) == 0:
-			return True
-		return False
-
-	def __ne__ (self, other):
-			return not self.__eq__(other)
-
-
-	def __hash__ (self):
-		return hash(self.data)
-
-class Edge(object):
-	def __init__ (self, from_node, to_node, data):
-		self.data = data
-		self.from_node = from_node
-		self.to_node = to_node
-
-	def __cmp__ (self, other):		
-		if self.data < other.data: return -1
-		if self.data == other.data: return 0
-		return 1
-
-	def __repr__ (self):
-		return "| data: " + str (self.data) + ", from node: " +  str (self.from_node.data) + ", to node: " + str (self.to_node.data) + " |"
-
-	def __hash__ (self):
-		return hash(self.data) ^ hash( (self.to_node, self.from_node) )
-
-class Graphx(object):
+class Graphx:
 	def __init__ (self, is_directed = False):
-		self.edges = set()
-		self.nodes = set()
 		self.graph = {}
 		self.is_directed = is_directed
+		self.full_connected = False
 
-	def add_edge (self, from_node, to_node, distance = 0, by_data = True):
-		if by_data:
-			from_node = self.find_node(from_node)			
-			to_node = self.find_node(to_node)
-		if from_node is None or to_node is None:			
-			return False
-		new_edge = Edge(from_node, to_node, distance)
-		from_node.edges.add (new_edge)
-		self.edges.add (new_edge)
-		if self.is_directed is False:
-			other_edge = Edge(to_node, from_node, distance)
-			to_node.edges.add(other_edge)
-			self.edges.add (other_edge)
-		return True
+	def add_node (self, data):
+		self.graph[data] = {}
 
-	def add_node (self, data, by_data = True):
-		if by_data:
-			node = Node (data)
-			self.nodes.add ( node )
-			return node
-		else:
-			self.nodes.add (data)
-			return data
+	def add_edge (self, data, from_node, to_node):
+		try:
+			tmp1, tmp2 = self.graph[from_node], self.graph[to_node]			
+		except:
+			raise BaseException("'from_node' or 'to_node' doesn't exist.")
+		tmp1.update({to_node:data})
+		if not self.is_directed:
+			tmp2.update({from_node:data})
 
-	def add_nodes_from_list (self, L):
-		for node in L:
-			self.add_node (node)
+	def edges (self): return graph_to_tuple (self.graph)
 
-	def get_best_path (self, begin, target):
-		begin_node = self.find_node(begin)
-		target_node = self.find_node(target)
-		if begin_node == None or target_node == None:
-			raise BaseException("Begin or target node doesn't exist.")
-		matrix = {}		
-		for i_node in self.nodes:
-			tmp = {}
-			for j_node in self.nodes:
-				tmp[j_node] = float ("inf")
-			matrix[i_node] = tmp
-
-		for edge in self.edges:
-			print edge
-		# 	matrix[edge.from_node][edge.to_node] = edge.data
-		# 	matrix[edge.to_node][edge.from_node] = edge.data
-
-		# for i_node in self.nodes:
-		# 	for j_node in self.nodes:
-		# 		for k_node in self.nodes:
-		# 			dt = matrix[i_node][j_node] + matrix[k_node][j_node]
-		# 			print dt
-		# 			if matrix[i_node][j_node] > dt:
-		# 				matrix[i_node][j_node] = dt
-
-		print matrix
-
-	def find_node (self, node, by_data = True):
-		for n in self.nodes:
-			if by_data:
-				if n.data == node: return n
-			else:
-				if n == node: return n
-		return None
-
-	def make_full_connected (self, fun = None):
-		if len(self.edges) > 0:
-			raise BaseException("** Error: Please delete all the edges first")
-		elif self.is_directed is True:
-			raise BaseException("** Error: Please make it digraf. Set atribute is_directed = False")
-		for i_node in self.nodes:
-			for j_node in self.nodes:
-				if i_node == j_node: continue
-				distance = 0 if fun == None else fun(i_node.data, j_node.data)
-				self.add_edge(i_node, j_node, by_data = False, distance = distance)
+	def make_full_connected (self, array = [], fun = None):		
+		for i in array: self.add_node(i)
+		nodes = self.graph.keys()
+		for i in nodes:
+			for j in nodes:
+				if i == j: continue
+				distance = 0 if fun == None else fun(i, j)
+				self.add_edge(distance, i, j)
 		self.full_connected = True
 
 	def make_knn (self, k):
-		new_edges = []
-		for node in self.nodes:
-			node.edges = set(sorted (node.edges, key = Node)[:k])
-			new_edges += node.edges
-		self.edges = set(new_edges)
-		self.is_directed = True
+		edges = graph_to_tuple(self.graph, key = lambda t: (t[1], t[0]))
+		counter = Counter()
+		for edge in edges:			
+			if counter[edge[1]] >= k:
+				del self.graph[edge[1]][edge[2]]
+			counter[edge[1]] += 1
 
 	def MST (self):
-		Y = list (self.nodes)
+		Y = self.graph.keys()
 		X = []
 		mst = Graphx()
-		edges = sorted(list(self.edges))
+		edges = graph_to_tuple(self.graph)
 		x = choice(Y)
 		X.append(x), Y.remove(x)
-		mst.add_node (x.data)
+		mst.add_node(x)
 		while len(Y) > 0:
-			for edge in edges:				
-				if edge.from_node in X and edge.to_node in Y:
-					mst.add_node (edge.to_node.data)
-					mst.add_edge (from_node = edge.from_node.data, to_node = edge.to_node.data, distance = edge.data)
-					X.append(edge.to_node), Y.remove(edge.to_node), edges.remove(edge)
+			for edge in edges:
+				distance, from_node, to_node = edge[0], edge[1], edge[2]
+				if (from_node in X) and (to_node in Y):
+					mst.add_node (to_node)
+					mst.add_edge (distance, from_node, to_node)
+					X.append(to_node), Y.remove(to_node), edges.remove(edge)
 					break
-				elif edge.to_node in X and edge.from_node in Y:
-					mst.add_node (edge.from_node.data)
-					mst.add_edge (from_node = edge.to_node.data, to_node = edge.from_node.data, distance = edge.data)
-					X.append(edge.from_node), Y.remove(edge.from_node), edges.remove(edge)
+				elif (to_node in X) and (from_node in Y):
+					mst.add_node (from_node)
+					mst.add_edge (distance, to_node, from_node)
+					X.append(from_node), Y.remove(from_node), edges.remove(edge)
 					break
 		return mst
 
+	def nodes(self): return self.graph.keys()
+
 if __name__ == '__main__':
 	g = Graphx()
-	n1 = g.add_node('a')
-	n2 = g.add_node('b')
-	n3 = g.add_node('c')
-	n4 = g.add_node('d')
-	g.make_full_connected()
-	# print g.nodes
-	print g.edges
+	g.graph = {'a': {'c': 0.06392587032381203, 'b': 0.23946025808650429, 'e': 0.06480403414170843, 'd': 0.0026144011652722288}, 'c': {'a': 0.06392587032381203, 'b': 0.24737540127027868, 'e': 0.8635607960307713, 'd': 0.00026374340418100495}, 'b': {'a': 0.23946025808650429, 'c': 0.24737540127027868, 'e': 0.9999847447746708, 'd': 0.5288620496272156}, 'e': {'a': 0.06480403414170843, 'c': 0.8635607960307713, 'b': 0.9999847447746708, 'd': 0.8739299535954806}, 'd': {'a': 0.0026144011652722288, 'c': 0.00026374340418100495, 'b': 0.5288620496272156, 'e': 0.8739299535954806}}
+	# g.make_full_connected(array = ['a', 'b', 'c', 'd', 'e'], fun = lambda a,b: random())
+	# print graph_to_tuple(g.graph)
+	print 'graph', g.graph
+	g.make_knn(k = 2)
+	print 'knn  ', g.graph
 
-	# print type (g)
-	# g.get_best_path(1, 2)
-	# print g.nodes
-	# g.add_edge(from_node = n1, to_node = n2, by_data = False, distance = 0.7)
-	# g.add_edge(from_node = n2, to_node = n1, by_data = False, distance = 0.3)
-	# g.add_edge(from_node = n2, to_node = n3, by_data = False, distance = 0.4)
-	# g.add_edge(from_node = n3, to_node = n2, by_data = False, distance = 0.5)
-	# g.add_edge(from_node = n2, to_node = n4, by_data = False, distance = 0.6)
-	# g.add_edge(from_node = n4, to_node = n2, by_data = False, distance = 0.8)
-	# g.add_edge(from_node = n1, to_node = n4, by_data = False, distance = 0.2)
-	# g.add_edge(from_node = n4, to_node = n1, by_data = False, distance = 0.4)
-	# g.add_edge(from_node = n4, to_node = n3, by_data = False, distance = 0.3)
-	g.make_knn(k = 1)
-	print "g.nodes", g.nodes
-	print "g.nodes", list(g.nodes)[2].edges
-	print "g.edges", g.edges
-	# print "--------------------- MST -----------------------"
+
 	# mst = g.MST()
-	# print "mst.nodes", mst.nodes
-	# print "mst.edges", mst.edges
-	# print "edges", g.edges
-	# print "-----------------------"
-	# print "nodes", g.nodes.sort()
+	# print 'mst  ', mst.graph
+	
